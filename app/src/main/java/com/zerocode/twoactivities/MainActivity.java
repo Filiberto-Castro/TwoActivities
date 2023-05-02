4c8d2b09cb84008619a72bc07bc7bfc4f7acd96a
Filiberto_Castro <fcastrob@miumg.edu.gt>
2023-05-02 12:45:39 -0600

    Return data back to the main Activity

--- app/src/main/java/com/zerocode/twoactivities/MainActivity.java
+++ app/src/main/java/com/zerocode/twoactivities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zerocode.twoactivities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,13 +8,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE =
             "com.codezero.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
     private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyMessageTextView = findViewById(R.id.text_message_reply);
 
     }
 
@@ -31,6 +38,25 @@ public class MainActivity extends AppCompatActivity {
         String message = mMessageEditText.getText().toString();
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        // Se lanza la actividad secundaria y se espera una respuesta
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    // Este método se llama cuando se recibe una respuesta de la actividad secundaria
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Se verifica que se reciba una respuesta de la actividad secundaria y que el código de solicitud coincida
+        if (requestCode == TEXT_REQUEST){
+            // Se verifica que la actividad secundaria haya finalizado correctamente
+            if (resultCode == RESULT_OK){
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyMessageTextView.setText(reply);
+                mReplyMessageTextView.setVisibility(View.VISIBLE);
+            }
+        }
+
     }
 }
